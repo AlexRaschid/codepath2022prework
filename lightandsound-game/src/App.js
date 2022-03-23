b1d8f150087253824df2fb7f0dcbd6fbef30e98f
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-23 15:59:53 -0400

    basic layout

---
+++ lightandsound-game/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Game from "./components/Game";
+
+const App = () => {
+    
+    const [gameState, setGameState] = useState(false)
+
+
+    return (
+        <div>
+            <div>
+                <h1>Light and Sound Memory game</h1>
+                <p>repeat the pattern back to win the game!</p>
+                <button id="startBtn" 
+                        onClick={() => {setGameState(!gameState)}}>
+                        {gameState ? "Stop" : "Start"}
+                </button>
+            </div>
+            <Game gameState={gameState}/>
+        </div>
+    );
+}
+
+export default App;
---
+++ lightandsound-game/src/components/Game.js
@@ -0,0 +1,18 @@
+const Game = (props) => {
+
+
+    return (
+        <div>
+            <table>
+               <tr>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                </tr> 
+            </table>
+        </div>
+    )
+}
+
+export default Game;
