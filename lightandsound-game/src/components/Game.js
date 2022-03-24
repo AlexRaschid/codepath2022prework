851b6cfe9b7436a56eef995eccbd1144e0b10afa
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-23 22:08:11 -0400

    im just going to follow the guide, I tried to do it in react and I just cant figure it out. useEffect is driving me crazy with these user inputs and checking which plays what blah blah brain hurts

--- lightandsound-game/src/components/Game.js
+++ lightandsound-game/src/components/Game.js
@@ -1,32 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Game = (props) => {
     
-    const [pattern, setPattern] = useState([2, 2, 4, 3, 2, 1, 2, 4]);
-    const [progress, setProgress] = useState(0);
-    const [sounds, setSounds] = useState([
-                                         new Audio("https://firebasestorage.googleapis.com/v0/b/codepath2022prework.appspot.com/o/audio%2Ffart1.mp3?alt=media&token=851a7e22-8008-4e11-b99c-38863b43a79c"),
-                                         new Audio("https://firebasestorage.googleapis.com/v0/b/codepath2022prework.appspot.com/o/audio%2Ffart2.mp3?alt=media&token=96b3b758-45ba-4620-9deb-0e68271d6bd2"),
-                                         new Audio("https://firebasestorage.googleapis.com/v0/b/codepath2022prework.appspot.com/o/audio%2Ffart3.mp3?alt=media&token=67cb6320-3a90-4c84-850b-da92af235ef6"),
-                                         new Audio("https://firebasestorage.googleapis.com/v0/b/codepath2022prework.appspot.com/o/audio%2Ffart4.mp3?alt=media&token=544dca10-0686-43a4-9c8a-45dee31ab22f")
+    let sounds = props.sounds;
 
-                                        ])
+         
+
     return (
         <div>
             <table>
                 <tbody>
                     <tr>
                         <th className="buttonCol">
-                            <button onClick={() => {sounds[0].play();}} className="gameButton" />       
+                            <button id='0' onClick={() => {props.playSound(0)}} className="btn1 gameButton" />       
                         </th>
                         <th className="buttonCol">
-                            <button onClick={() => {sounds[1].play();}} className="gameButton"></button>
+                            <button id='1' onClick={() => {props.playSound(1)}} className="btn2 gameButton"></button>
                         </th>
                         <th className="buttonCol">
-                            <button onClick={() => {sounds[2].play();}} className="gameButton"></button>
+                            <button id='2' onClick={() => {props.playSound(2)}} className="btn3 gameButton"></button>
                         </th>
                         <th className="buttonCol">
-                            <button onClick={() => {sounds[3].play();}} className="gameButton"></button>
+                            <button id='3' onClick={() => {props.playSound(3)}} className="btn4 gameButton"></button>
                         </th>
                     </tr> 
                 </tbody>
